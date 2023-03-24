e1c4d09be3d4354f542596b650654694f591ac0d
Korotin_P <74100670+he111z@users.noreply.github.com>
2023-03-24 21:54:40 +0400

    Update L_W_2.py

--- L_W_2.py
+++ L_W_2.py
@@ -8,15 +8,14 @@ while True:
     s=text.readline()
     if s=='':
         break
-    res_s=re.findall('\d+', s)
-    for i in range(len(res_s)):
-        if int((res_s[i])[0])==0:
-            res_s[i]=res_s[i].replace('0', '', 1)
-        if int(res_s[i])%2==0 and len(res_s[i])%2==1 and len(res_s[i])>2:
-            print(res_s[i])
-            for j in range(len(res_s[i])):
-                if not((res_s[i])[j] in num):
-                    num.append((res_s[i])[j])
+    res_s=(re.findall('[\d]*[\d]{%s}[02468]'%2, s))
+    if len(res_s)>0:
+        for i in range(len(res_s)):
+            if len(res_s[i])%2==1:
+                print(res_s[i])
+                for j in range(len(res_s[i])):
+                    if not((res_s[i])[j] in num):
+                        num.append((res_s[i])[j])
 for i in range(len(num)):
     num[i]=num[i].replace(num[i], fntw[int(num[i])], 1)
 if len(num)>0:
